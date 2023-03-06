1e5cb11f9a47f55811657c8c3bf3bcb16d17e9eb
RMRKBL <johnson.tajh11@gmail.com>
2023-03-05 21:55:35 -0600

    finished auth flow

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+const { cookieKey } = require("./config/keys");
+require("./models/user");
+require("./services/passport");
+mongoose.set("strictQuery", true);
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(keys.mongoURI);
+}
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ goodbye: "friend" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-const PORT = process.env.PORT || 3000;
+require("./routes/authRoutes")(app);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("App is serving!");
 });
---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: {
+    type: String,
+    // required: true,
+    // unique: true,
+  },
+});
+
+mongoose.model("users", userSchema);
